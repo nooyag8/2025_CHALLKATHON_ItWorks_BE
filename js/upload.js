d01eebc7b4576974da6664507e4badc5d1701baa
seojeong <sjeong11ee@gmail.com>
2025-06-26 02:33:35 +0900

    사진 업데이트 가능

---
+++ js/upload.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadDir = path.join(__dirname, "..", "uploads");
+
+// uploads 폴더가 없으면 생성
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
