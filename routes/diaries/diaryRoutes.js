16d56b6743345b6d20d672448a5fa7cac0dbd321
seojeong <sjeong11ee@gmail.com>
2025-06-25 15:12:01 +0900

    일기 db 연동 완료

--- routes/diaries/diaryRoutes.js
+++ routes/diaries/diaryRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const diaryController = require("../../controllers/diaryController");
 
 // 날짜로 일기 조회
-router.get("/date/:date", diaryController.getDiaryByDate);
+router.get("/date/:date", (req, res, next) => {
+  console.log("📅 요청된 날짜:", req.params.date);
+  next();
+}, diaryController.getDiaryByDate);
 
 router.get("/status", diaryController.getStatus);
 router.post("/auto-save", diaryController.autoSave);
